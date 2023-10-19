8c2f10c58273bedc8075b33d4154cb6a1e58d7e8
TheColorman <thoren.alex@gmail.com>
2023-10-19 15:55:59 +0200

    Add dbHelpers import and define Locals interface with db property. Also define Recipe and Ingredient types.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,57 @@
+import { dbHelpers } from './hooks.server';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			db: typeof dbHelpers;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
 }
+
+export type Recipe = {
+	averageRating?: number;
+	description?: string;
+	difficulty?: number;
+	headline?: string;
+	id: string;
+	imagePath?: string;
+	slug?: string;
+	name?: string;
+	prepTime?: string;
+	totalTime?: string;
+	recipeYield?: string;
+};
+
+export type IngredientID = string;
+export type Ingredient = {
+	id: IngredientID;
+	uuid: string;
+	slug: string;
+	type: string;
+	country: string;
+	imageLink?: string;
+	imagePath?: string;
+	name: string;
+	internalName?: string;
+	shipped: boolean;
+	description?: string;
+	usage: number;
+	hasDuplicatedName?: boolean;
+	allergens: AllergenID[];
+	family: Family;
+};
+export type IngredientPartial = {
+	id: IngredientID;
+	slug: string;
+	type: string;
+	name: string;
+	internalName?: string;
+	imagePath?: string;
+};
 
 export {};
